902dc182d16c331ae6393cd8b55f3b0b165c9592
Maurizio Ruchay <maurizio.ruchay@syss.de>
2020-11-12 13:18:39 +0100

    Add VS Code

---
+++ code/install-extensions.sh
@@ -0,0 +1,9 @@
+code --install-extension analytic-signal.preview-pdf && 
+code --install-extension ban.spellright && 
+code --install-extension corentinartaud.pdfpreview && 
+code --install-extension James-Yu.latex-workshop && 
+code --install-extension streetsidesoftware.code-spell-checker && 
+code --install-extension streetsidesoftware.code-spell-checker-german && 
+code --install-extension tomoki1207.pdf && 
+code --install-extension torn4dom4n.latex-support && 
+echo SCCESS
